c56bc9a1d9c18860f4025c8cc992d1ecf257efcc
brunostracke <67613428+brunostracke@users.noreply.github.com>
2020-11-04 19:42:09 -0300

    Add files via upload

---
+++ SpringBootEssentials-1/src/main/java/teste/error/CustomErrorType.java
@@ -0,0 +1,12 @@
+package teste.error;
+
+public class CustomErrorType {
+
+	private String errorMessage;
+
+	public CustomErrorType(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+	
+	
+}
--- SpringBootEssentials-1/src/main/java/teste/model/Student.java
+++ SpringBootEssentials-1/src/main/java/teste/model/Student.java
@@ -1,12 +1,62 @@
 package teste.model;
 
+import static java.util.Arrays.asList;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
 	
 	private String name;
+	private int id;
+	public static List<Student> studentList;
+	
+	static {
+		studentRepository();
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	public Student() {
 	}
 
+	public Student(int id, String name) {
+		this.name = name;
+		this.id = id;
+	}
+
+	private static void studentRepository() {
+		studentList = new ArrayList<>(asList(new Student (1, "Deku"), new Student (2, "Todoroki")));
+	}
+	
 	public Student(String name) {
 		this.setName(name);
 	}
